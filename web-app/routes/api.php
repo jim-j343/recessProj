9b5afd634e1b1424a6b8db77a050aeca8fbd12d7
Caleb Cheptoris <calebcheptoris@gmail.com>
2026-07-12 21:26:32 +0300

    feat: add API routes for groups, quizzes and participation grading

--- web-app/routes/api.php
+++ web-app/routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ForumController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\GroupApiController;
+use App\Http\Controllers\Api\QuizApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,4 +29,20 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/topics',                [ForumController::class, 'store']);
     Route::get('/topics/{topic}',         [ForumController::class, 'show']);
     Route::post('/topics/{topic}/posts',  [ForumController::class, 'storePost']);
+
+        // Groups
+    Route::get('/groups', [GroupApiController::class, 'index']);
+    Route::post('/groups', [GroupApiController::class, 'store']);
+    Route::post('/groups/{group}/join', [GroupApiController::class, 'join']);
+    Route::get('/groups/{group}/members', [GroupApiController::class, 'members']);
+    Route::patch('/groups/{group}/members/{userId}/approve', [GroupApiController::class, 'approve']);
+
+    // Quizzes
+    Route::get('/quizzes', [QuizApiController::class, 'index']);
+    Route::get('/quizzes/my', [QuizApiController::class, 'myQuizzes']);
+    Route::get('/quizzes/{id}', [QuizApiController::class, 'show']);
+    Route::post('/quizzes/{id}/submit', [QuizApiController::class, 'submit']);
+    Route::get('/quizzes/{id}/results', [QuizApiController::class, 'myResult']);
+    Route::get('/quizzes/{id}/all-results', [QuizApiController::class, 'allResults']);
+
 });
