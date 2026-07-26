1e1e16a0ea717af7becdabf4410a2a94c982596a
Caleb Cheptoris <calebcheptoris@gmail.com>
2026-07-26 16:28:28 +0300

    fix(web): improve Blade layouts for testing environment

--- web-app/resources/views/welcome.blade.php
+++ web-app/resources/views/welcome.blade.php
@@ -12,7 +12,9 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700&display=swap" rel="stylesheet" />
 
         <!-- Styles / Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @unless(app()->runningUnitTests())
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @endunless
     </head>
     <body class="font-sans text-gray-900 antialiased bg-white">
 
